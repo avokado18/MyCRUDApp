e7cc373556a2cf28b0e33668c116ed8e49c3a14e
avokado18 <proskuryakova1996@gmail.com>
2016-09-28 14:40:38 +0300

    ready

--- web/test/controller.js
+++ web/test/controller.js
@@ -9,28 +9,60 @@ clientApp.factory('Clients',[
 ]);
 clientApp.controller('ClientList', function ($scope, $http, Clients) {
     $scope.clients = Clients.query();
-    $scope.edit = -1;
-    $scope.Add = function () {
-        Clients.save($scope.newclient);
-        $scope.clients = Clients.query();
+    $scope.AddMessage = 'Add new client';
+
+    $scope.AddClick = function () {
+        $scope.showPopUpDialog = true;
+        $scope.ifTrueThenAdd = true;
+        $scope.AddOrEditMessage = $scope.AddMessage;
     };
     $scope.Delete = function (id) {
-        var ans = confirm('Are you sure to delete it?');
-        if (ans){
-            Clients.delete({clientId: id});
-        }
+        Clients.delete({clientId:id});
         $scope.clients = Clients.query();
-    };
-    $scope.EditClick = function (id) {
-        $scope.edit = id;
-        $scope.oldclient = Clients.get({clientId: id});
-    };
-    $scope.Edit = function () {
-        $scope.edit = id;
-        $scope.oldclient = Clients.get({clientId: id});
-        Clients.update($scope.oldclient);
-        $scope.edit = -1;
-        $scope.clients = Clients.query();
-    };
+        $route.reload();
+    }
 });
-
+clientApp.directive('popUpDialog', function () {
+    return{
+        restrict:'E',
+        scope:false,
+        templateUrl:'/test/addEditForm.html',
+        controller:function($scope, $route, Clients){
+            var EditMessage = 'Edit client';
+            $scope.AddOrEditMessage = $scope.AddMessage;
+            $scope.showPopUpDialog = false;
+            $scope.ifTrueThenAdd = true;
+            $scope.EditClick = function (id) {
+                $scope.showPopUpDialog = true;
+                $scope.ifTrueThenAdd = false;
+                $scope.AddOrEditMessage = EditMessage;
+                $scope.newclient = Clients.get({clientId:id});
+            }
+            $scope.AddOrEdit = function (add) {
+                $scope.newclient.birthday = document.getElementById('_birthday').value;
+                if (add)
+                    $scope.Add();
+                else
+                    $scope.Edit();
+                $scope.showPopUpDialog = false;
+            }
+            $scope.Add = function () {
+                document.getElementById('myForm').reset();
+                Clients.save($scope.newclient);
+                $scope.clients = Clients.query();
+                $route.reload();
+            }
+            $scope.Edit = function () {
+                document.getElementById('myForm').reset();
+                Clients.update($scope.newclient);
+                $scope.edit = -1;
+                $scope.clients = Clients.query();
+                $route.reload();
+            }
+            $scope.closePopUpDialog = function(){
+                document.getElementById('myForm').reset();
+                $scope.showPopUpDialog = false;
+            }
+        }
+    }
+});
